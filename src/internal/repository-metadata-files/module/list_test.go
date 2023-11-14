c83abe03fb54c5ca494f9aca63c3c75d136b3ff3
James Humphries <james@james-humphries.co.uk>
2023-11-14 17:35:34 +0000

    Simplify, and document the regex
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

---
+++ src/internal/repository-metadata-files/module/list_test.go
@@ -0,0 +1,73 @@
+package module
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"registry-stable/internal/module"
+)
+
+func Test_ExtractModuleDetailsFromPath(t *testing.T) {
+	type TestCase struct {
+		name           string
+		input          string
+		expectedOutput *module.Module
+	}
+
+	testCases := []TestCase{
+		{
+			name:  "Valid module path",
+			input: "modules/t/terraform-aws-modules/lambda/aws.json",
+			expectedOutput: &module.Module{
+				Namespace:    "terraform-aws-modules",
+				Name:         "lambda",
+				TargetSystem: "aws",
+			},
+		},
+		{
+			name:  "Valid module path with numbers",
+			input: "modules/t/terraform-aws-modules1234/lambda1234/aws1234.json",
+			expectedOutput: &module.Module{
+				Namespace:    "terraform-aws-modules1234",
+				Name:         "lambda1234",
+				TargetSystem: "aws1234",
+			},
+		},
+		{
+			name:           "Invalid module path (no .json)",
+			input:          "modules/t/terraform-aws-modules/lambda/aws",
+			expectedOutput: nil,
+		},
+		{
+			name:           "Invalid module path (empty)",
+			input:          "",
+			expectedOutput: nil,
+		},
+		{
+			name:           "Invalid module path (missing namespace)",
+			input:          "modules/t//lambda/aws.json",
+			expectedOutput: nil,
+		},
+		{
+			name:           "Invalid module path (missing name)",
+			input:          "modules/t/terraform-aws-modules//aws.json",
+			expectedOutput: nil,
+		},
+		{
+			name:           "Invalid module path (missing target system)",
+			input:          "modules/t/terraform-aws-modules/lambda/.json",
+			expectedOutput: nil,
+		},
+		{
+			name:           "Invalid module path (missing namespace, name, and target system)",
+			input:          "modules/t///.json",
+			expectedOutput: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.expectedOutput, extractModuleDetailsFromPath(tc.input), "Extracted module details do not match expected output")
+		})
+	}
+}
