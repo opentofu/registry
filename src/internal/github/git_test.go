7e6a5a54a3283efb730d067786a8614d87d15e52
Christian Mesh <christianmesh1@gmail.com>
2023-11-15 13:50:12 -0500

    Simpler test assertions

--- src/internal/github/git_test.go
+++ src/internal/github/git_test.go
@@ -1,16 +1,17 @@
 package github
 
 import (
+	"errors"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_parseTagsFromStdout(t *testing.T) {
 	cases := map[string]struct {
 		input       []string
 		expected    []string
-		expectedErr string
+		expectedErr error
 	}{
 		"Simple Tag": {
 			input:    []string{"314159265358979     refs/tags/v0.0.1"},
@@ -26,19 +27,19 @@ func Test_parseTagsFromStdout(t *testing.T) {
 		},
 		"Missing Field": {
 			input:       []string{"borkborkborkrefs/tags/"},
-			expectedErr: "Invalid format for tag 'borkborkborkrefs/tags/', expected two fields",
+			expectedErr: errors.New("Invalid format for tag 'borkborkborkrefs/tags/', expected two fields"),
 		},
 		"Extra Field": {
 			input:       []string{"bork bork refs/tags/"},
-			expectedErr: "Invalid format for tag 'bork bork refs/tags/', expected two fields",
+			expectedErr: errors.New("Invalid format for tag 'bork bork refs/tags/', expected two fields"),
 		},
 		"Missing tags/refs": {
 			input:       []string{"314159265358979refs/tags/   v0.0.1"},
-			expectedErr: "Invalid format for tag '314159265358979refs/tags/   v0.0.1', expected 'refs/tags/' prefix",
+			expectedErr: errors.New("Invalid format for tag '314159265358979refs/tags/   v0.0.1', expected 'refs/tags/' prefix"),
 		},
 		"Missing version": {
 			input:       []string{"314159265358979 refs/tags/"},
-			expectedErr: "Invalid format for tag '314159265358979 refs/tags/', no version provided",
+			expectedErr: errors.New("Invalid format for tag '314159265358979 refs/tags/', no version provided"),
 		},
 	}
 
@@ -46,21 +47,8 @@ func Test_parseTagsFromStdout(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			out, err := parseTagsFromStdout(tc.input)
 
-			if tc.expectedErr == "" {
-				if err != nil {
-					t.Errorf("Unexpected error: %v", err)
-				}
-			} else {
-				if err == nil {
-					t.Errorf("Expected error %s, got <nil>", tc.expectedErr)
-				} else if err.Error() != tc.expectedErr {
-					t.Errorf("Expected error %s, got %v", tc.expectedErr, err)
-				}
-			}
-
-			if diff := cmp.Diff(out, tc.expected); diff != "" {
-				t.Errorf("Expected %v, got %v: %s", tc.expected, out, diff)
-			}
+			assert.Equal(t, tc.expectedErr, err)
+			assert.Equal(t, tc.expected, out)
 
 		})
 	}
