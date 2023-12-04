e64f8a216a3c6e670eb0f75c7d2a7bcaf695d3c9
Roni Frantchi <roni.frantchi@gmail.com>
2023-12-04 15:03:38 +0200

    drop any dependencies

--- .github/validator/provider-repository.js
+++ .github/validator/provider-repository.js
@@ -6,53 +6,39 @@
  * @returns {Promise<string>} An error message or `'success'`
  */
 module.exports = async (field) => {
-    const { Octokit } = require('@octokit/rest')
-    const core = require('@actions/core')
-
-    const github = new Octokit({
-        auth: core.getInput('github-token', { required: true })
-    })
+    const core = require('@actions/core');
 
     if (typeof field !== 'string') return 'Field type is invalid';
 
     if (!/^[a-zA-Z0-9-]+\/terraform-provider-[a-zA-Z0-9-]+$/.test(field))
         return 'Repository must be a valid terraform provider repo matching the pattern "<org>/<repo>/terraform-provider-<name>"';
 
-    try {
-        // Check if the provider repository exists
-        core.info(`Checking if repository '${field}' exists`)
-        const [owner, repo] = field.split('/');
+    // Check if the provider repository exists
+    core.info(`Checking if repository '${field}' exists`);
+    const [owner, repo] = field.split('/');
 
-        await github.rest.repos.get({
-            owner,
-            repo,
-        });
-
-        try {
-            await github.rest.repos.getContent({ owner: 'opentofu', repo: 'registry', path: `providers/${owner.charAt(0)}/${owner}/${repo}.json` });
-
-            // if we did not get 404 from github - the provider already exists
-            core.error(`Repository '${field}' already exists in the registry`)
-            return `Provider '${field}' already exists in the registry`
-        } catch (error) {
-            if (error.status === 404) {
-                // the provider does not exist in the registry, so good submission
-            } else {
-                // Otherwise, something else went wrong...
-                throw error
-            }
-        }
-
-        core.info(`Repository '${field}' exists`)
-        return 'success'
-    } catch (error) {
-        if (error.status === 404) {
+    // Check if the repository exists
+    const repoResponse = await fetch(`https://api.github.com/repos/${owner}/${repo}`);
+    if (!repoResponse.ok) {
+        if (repoResponse.status === 404) {
             // If the repo does not exist, return an error message
-            core.error(`Repository '${field}' does not exist`)
-            return `Repository '${field}' does not exist`
+            core.error(`Repository '${field}' does not exist`);
+            return `Repository '${field}' does not exist`;
         } else {
-            // Otherwise, something else went wrong...
-            throw error
+            throw new Error(`Failed to check repository existence: ${repoResponse.statusText}`);
         }
     }
-}
+
+    // Check if the provider repository exists in the registry
+    const registryResponse = await fetch(`https://raw.githubusercontent.com/opentofu/registry/main/providers/${owner.charAt(0)}/${owner}/${repo}.json`);
+    if (registryResponse.ok) {
+        // If the provider exists in the registry, return an error message
+        core.error(`Repository '${field}' already exists in the registry`);
+        return `Provider '${field}' already exists in the registry`;
+    } else if (registryResponse.status !== 404) {
+        throw new Error(`Failed to check registry: ${registryResponse.statusText}`);
+    }
+
+    core.info(`Repository '${field}' exists`);
+    return 'success';
+};
