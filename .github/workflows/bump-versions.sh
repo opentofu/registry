c474e882e8e94aef4ed1650dfaaf686f866333ae
James Humphries <james@james-humphries.co.uk>
2026-08-05 15:25:28 +0100

    Limit GHA workflows to specific paths, add bump-versions.sh to shellcheck too
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- .github/workflows/bump-versions.sh
+++ .github/workflows/bump-versions.sh
@@ -2,35 +2,35 @@
 
 set +x
 
-if [ -z "${PREFIX}" ]; then
+if [[ -z "${PREFIX}" ]]; then
 	echo "Expecting \$PREFIX to be set"
 	exit 1
 fi
-if [ -z "${GITHUB_REF_NAME}" ]; then
+if [[ -z "${GITHUB_REF_NAME}" ]]; then
 	echo "Expecting \$GITHUB_REF_NAME to be set"
 	exit 1
 fi
-if [ -z "${GITHUB_RUN_NUMBER}" ]; then
+if [[ -z "${GITHUB_RUN_NUMBER}" ]]; then
 	echo "Expecting \$GITHUB_RUN_NUMBER to be set"
 	exit 1
 fi
-if [ -z "${ENVIRONMENT}" ]; then
+if [[ -z "${ENVIRONMENT}" ]]; then
 	echo "Expecting \$ENVIRONMENT to be set"
 	exit 1
 fi
 
-cd ./src
+cd ./src || exit 1
 go run ./cmd/bump-versions -module-namespace "${PREFIX}" -provider-namespace "${PREFIX}"
-cd ../
+cd ../ || exit 1
 
 BRANCH="${GITHUB_REF_NAME}-bump-versions-${GITHUB_RUN_NUMBER}"
-if [ "${ENVIRONMENT}" = "Production" ]; then
+if [[ "${ENVIRONMENT}" = "Production" ]]; then
 	export BRANCH="${GITHUB_REF_NAME}"
 fi
 
 # Try to get to the latest HEAD
-git fetch origin $BRANCH
-git checkout -b $BRANCH
+git fetch origin "${BRANCH}"
+git checkout -b "${BRANCH}"
 
 # Commit changes
 git status
@@ -40,17 +40,16 @@ git config user.name "OpenTofu Core Development Team"
 git commit -m "Automated bump of versions for providers and modules (${PREFIX})"
 
 # Racing with other jobs, try a few times to push changes
-for i in {0..30}; do
-	git push -u origin $BRANCH
-	if [ $? = 0 ]; then
+for _ in {0..30}; do
+	if git push -u origin "${BRANCH}"; then
 		echo "Providers and modules changes were pushed to branch: ${BRANCH}"
 		exit 0
 	fi
 	sleep 1
-	git fetch origin $BRANCH
-	git rebase origin/$BRANCH
+	git fetch origin "${BRANCH}"
+	git rebase origin/"${BRANCH}"
 done
 
 echo "Failed to push providers and modules changes to branch: ${BRANCH}"
-exit -1
+exit 1
 
